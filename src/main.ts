08e81c931d15acbce3172a273b5df85cc836584c
朱力涛 <643612824@qq.com>
2024-10-07 09:59:29 +0800

    加了pinia持久化，设置信息会存储在浏览器端的localstorage，页面刷新设置信息不会刷新

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import router from '@/router'
 //primevue
 import PrimeVue from 'primevue/config';
@@ -8,11 +9,10 @@ import 'primevue/resources/themes/saga-blue/theme.css';
 import 'primevue/resources/primevue.min.css';           
 import 'primeicons/primeicons.css';                     
 import 'primeflex/primeflex.css';                  
-//mathjax     
-import MathJax from 'mathjax/es5/tex-mml-chtml.js';
 
 const pinia = createPinia();
 const app = createApp(App);
+pinia.use(piniaPluginPersistedstate)
 app
   .use(router)
   .use(pinia)
--- src/stores/settingStore.ts
+++ src/stores/settingStore.ts
@@ -14,5 +14,6 @@ export const useSettingStore = defineStore('settingStore', {
     currencySymbol: (state) => getCurrencySymbol(state.currencyType),
     timeUnitText: (state) => getTimeUnitText(state.timeUnit),
     isCompound: (state) => state.interestMethod === 'compound'
-  }
+  },
+  persist: true
 });
