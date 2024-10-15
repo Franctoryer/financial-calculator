f78c58ac7e9b4ceb0372e90c4d425ee76de1d271
Choihyobin111 <454974197@qq.com>
2024-10-15 18:48:38 +0800

    slogan明暗变化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,28 @@
 <template>
   <div class="main">
-    <div class="slogan">
+    <div :class="isDark ? 'slogan-dark-theme' : 'slogan-light-theme'">
       一键计算，财富增长
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+  import { storeToRefs } from "pinia"
+  import { useThemeStore } from "@/stores/themeStore";
+
+  const { themeClass, isDark } = storeToRefs(useThemeStore());
 </script>
 
 <style scoped>
-.slogan {
+.slogan-light-theme {
+  width: 100%;
+  font-size: 30px;
+  text-align: center;
+  font-weight: bold;
+  margin-top: 30vh;
+}
+.slogan-dark-theme {
+  color:aliceblue;
   width: 100%;
   font-size: 30px;
   text-align: center;
