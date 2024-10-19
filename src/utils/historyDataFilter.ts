0b90c799d979c9a971ba08e515305422fb29eb11
朱力涛 <643612824@qq.com>
2024-10-19 18:15:35 +0800

    修复all下不能正常删除的bug

--- src/utils/historyDataFilter.ts
+++ src/utils/historyDataFilter.ts
@@ -3,7 +3,20 @@ import type { HistoryDataView } from '@/types/HistoryDataView';
 
 export const getHistoryByTag = (historyData: HistoryData[], tag: string): HistoryDataView[] => {
   if (tag === 'all') {
-    return historyData;
+    let resultData = [] as HistoryDataView[];
+    let index = 0;
+    for (const history of historyData) {
+      let resultElement: HistoryDataView = {
+        index: index,
+        saveTime: history.saveTime,
+        name: history.name,
+        inputData: history.inputData,
+        resultData: history.resultData
+      }
+      resultData.push(resultElement);
+      index++;
+    }
+    return resultData;
   }
   let resultData = [] as HistoryDataView[];
   let index: number = 0;
