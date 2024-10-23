cbe96cb47c7457c509d3d126e0b140ba22036d06
朱力涛 <643612824@qq.com>
2024-10-23 12:35:00 +0800

    修改货币的变量类型

--- src/stores/input/CurrencyInputStore.ts
+++ src/stores/input/CurrencyInputStore.ts
@@ -3,8 +3,8 @@ import { defineStore } from "pinia";
 export const useCurrencyInputStore = defineStore("CurrencyInputStore", {
   state: () => ({
     money: 1,
-    fromCurrency: "CNY",
-    toCurrency: "USD"
+    fromCurrency: "CNY" as string | null,
+    toCurrency: "USD" as string | null
   }),
   persist: {
     storage: sessionStorage
